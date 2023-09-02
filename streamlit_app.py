bc14c1c537fee5337efab99535a1d9fc79ca6b9d
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-02 15:15:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout = 'wide')
+
 st.title('ChatCSV with LLM')
 
 input_csv = st.file_uploader('Upload your CSV file', type = ['csv'])
