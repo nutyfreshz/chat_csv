0f31072d14ab6c0273d9b7ce91aefd7b1ef7cf27
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-02 15:44:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ input_csv = st.file_uploader('Upload your CSV file', type = ['csv'])
 if input_csv is not None:
   col1, col2 = st.columns([1,1])
 
-with col1:
-  st.info('CSV Uploaded success fully')
-  data = pd.read_csv(input_csv)
-  st.DataFrame(data)
+  with col1:
+    st.info('CSV Uploaded success fully')
+    data = pd.read_csv(input_csv)
+    st.DataFrame(data)
