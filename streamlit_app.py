dadec10167d3f7c39453499bea8ab459640a72d4
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-06 11:42:26 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout = 'wide')
+
+st.title('ChatCSV with LLM')
+
+input_csv = st.file_uploader('Upload your CSV file', type = ['csv'])
+
+if input_csv is not None:
+  col1, col2 = st.columns([1,1])
+
+  with col1:
+    st.info('CSV Uploaded success fully')
+    data = pd.read_csv(input_csv)
+    st.dataframe(data)
