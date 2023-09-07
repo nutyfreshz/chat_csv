ce58126a52c949288959bc47dc763d738c01658a
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-07 16:01:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,46 @@
-import subprocess
-import streamlit as st
-import pandas as pd
-import os
-from dotenv import load_dotenv
+import streamlit as st 
 from pandasai.llm.openai import OpenAI
+from dotenv import load_dotenv
+import os
+import pandas as pd
 from pandasai import PandasAI
-import shelve
 
 load_dotenv()
 
+
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-def chat_with_csv(df, prompt):
-    try:
-        llm = OpenAI(api_token=openai_api_key)
-        pandas_ai = PandasAI(llm)
-        result = pandas_ai.run(df, prompt=prompt)
-        return result
-    except Exception as e:
-        return str(e)
 
-# ... rest of your code ...
+def chat_with_csv(df,prompt):
+    llm = OpenAI(api_token=openai_api_key)
+    pandas_ai = PandasAI(llm)
+    result = pandas_ai.run(df, prompt=prompt)
+    print(result)
+    return result
 
-# Specify a different database backend (e.g., 'dbm.gnu' or 'dbm.dumb')
-db_backend = 'dbm.gnu'  # You can try other backends as well
+st.set_page_config(layout='wide')
 
-# Open the shelve database with the specified backend
-with shelve.open('mydata.db', 'c', protocol=4, writeback=True, flag='n', backend=db_backend) as db:
-    # Perform operations on the database
+st.title("ChatCSV powered by LLM")
+
+input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+
+if input_csv is not None:
+
+        col1, col2 = st.columns([1,1])
+
+        with col1:
+            st.info("CSV Uploaded Successfully")
+            data = pd.read_csv(input_csv)
+            st.dataframe(data, use_container_width=True)
+
+        with col2:
+
+            st.info("Chat Below")
+            
+            input_text = st.text_area("Enter your query")
+
+            if input_text is not None:
+                if st.button("Chat with CSV"):
+                    st.info("Your Query: "+input_text)
+                    result = chat_with_csv(data, input_text)
+                    st.success(result)
