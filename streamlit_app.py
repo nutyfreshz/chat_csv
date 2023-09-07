df47b88625fa8ce2c33ab13d832642f32cae3b66
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-07 16:01:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,30 @@
-import streamlit as st 
-from pandasai.llm.openai import OpenAI
-from dotenv import load_dotenv
-import os
+import subprocess
+import streamlit as st
 import pandas as pd
+import os
+from dotenv import load_dotenv
+from pandasai.llm.openai import OpenAI
 from pandasai import PandasAI
+import shelve
 
 load_dotenv()
 
-
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+def chat_with_csv(df, prompt):
+    try:
+        llm = OpenAI(api_token=openai_api_key)
+        pandas_ai = PandasAI(llm)
+        result = pandas_ai.run(df, prompt=prompt)
+        return result
+    except Exception as e:
+        return str(e)
 
-def chat_with_csv(df,prompt):
-    llm = OpenAI(api_token=openai_api_key)
-    pandas_ai = PandasAI(llm)
-    result = pandas_ai.run(df, prompt=prompt)
-    print(result)
-    return result
+# ... rest of your code ...
 
-st.set_page_config(layout='wide')
+# Specify a different database backend (e.g., 'dbm.gnu' or 'dbm.dumb')
+db_backend = 'dbm.gnu'  # You can try other backends as well
 
-st.title("ChatCSV powered by LLM")
-
-input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
-
-if input_csv is not None:
-
-        col1, col2 = st.columns([1,1])
-
-        with col1:
-            st.info("CSV Uploaded Successfully")
-            data = pd.read_csv(input_csv)
-            st.dataframe(data, use_container_width=True)
-
-        with col2:
-
-            st.info("Chat Below")
-            
-            input_text = st.text_area("Enter your query")
-
-            if input_text is not None:
-                if st.button("Chat with CSV"):
-                    st.info("Your Query: "+input_text)
-                    result = chat_with_csv(data, input_text)
-                    st.success(result)
+# Open the shelve database with the specified backend
+with shelve.open('mydata.db', 'c', protocol=4, writeback=True, flag='n', backend=db_backend) as db:
+    # Perform operations on the database
